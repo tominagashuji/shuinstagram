a4e00e0a3cb6149855cbb96508bda47096327963
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2018-03-03 15:31:15 +0900

    instacloan_kadai

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,6 +2,6 @@ class PictureMailer < ApplicationMailer
   def picture_mail(picture)
    @picture = picture
 
-   mail to: "shuji_tominaga@diveintocode.jp", subject: "お問い合わせの確認メール"
+   mail to: "shuji_tominaga@diveintocode.jp", subject: "投稿確認メールざます！！＾＾"
   end
 end
